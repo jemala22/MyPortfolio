b482c32540f5661d45ffe40523c08d5d4cfee5b3
Giorgi <bitsadzegiorgi9@gmail.com>
2023-01-25 17:19:17 +0400

    this one really better

--- script.js
+++ script.js
@@ -22,4 +22,17 @@ $(document).ready(function(){
       } // End if
     });
   });
-  
+// Get the loading element
+var loading = document.getElementById("loading");
+// Get the content element
+var content = document.getElementById("content");
+
+// When the page is loading, show the loading animation and hide the content
+window.addEventListener("load", function() {
+  loading.style.display = "block";
+  content.style.display = "none";
+  setTimeout(function() {
+    loading.style.display = "none";
+    content.style.display = "block";
+  }, 1500);
+});
